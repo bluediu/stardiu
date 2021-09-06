0a24b9565d1192aa192b714fa227184765352a6d
Josué Rivas <josuerivas2311@gmail.com>
2021-09-05 18:49:30 -0600

    start project :)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
