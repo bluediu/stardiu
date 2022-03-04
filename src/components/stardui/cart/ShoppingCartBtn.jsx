1ab81d1467319ce5af8b1b11641049f22c8e3ad2
Josue Rivas <reycoc2311@gmail.com>
2022-03-04 12:51:02 -0600

    refactor(cart): separated cart logic in a custom hook

--- src/components/stardui/cart/ShoppingCartBtn.jsx
+++ src/components/stardui/cart/ShoppingCartBtn.jsx
@@ -1,91 +1,17 @@
 import React, { useState } from 'react';
 import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  countProducts,
-  startAddToCart,
-  startDeleteOneFromCart,
-  startIsProductAddedToCart,
-} from '../../../context/actions/shoppingCart';
+
 import AuthModal from '../../modal/authModal/AuthModal';
-import { useEffect } from 'react';
+
+import { useCart } from '../../../hooks/useCart';
 
 function ShoppingCartBtn() {
-  const dispatch = useDispatch();
-
-  /* TODO: Refactor components creating a hook */
   const [showModal, setShowModal] = useState(false);
-  const [loadingAction, setLoadingAction] = useState(false);
-  const [product, setProduct] = useState(null);
-  const [productExist, setProductExist] = useState(false);
-
-  const { detailsData } = useSelector((state) => state.products);
-  const { total } = useSelector((state) => state.shoppingCart);
-  const { uid } = useSelector((state) => state.auth);
-
-  const handleAddToCart = () => {
-    if (uid) {
-      setLoadingAction(true);
-      dispatch(
-        startAddToCart({
-          userId: uid,
-          productId: detailsData._id,
-          quantity: 1,
-        })
-      );
-      dispatch(countProducts(total + 1));
-      verifyProduct();
-
-      setLoadingAction(false);
-    } else {
-      setShowModal(true);
-    }
-  };
-
-  const handleRemoveFromCart = () => {
-    if (uid) {
-      setLoadingAction(true);
-      startDeleteOneFromCart(detailsData._id, uid);
-
-      dispatch(countProducts(total - 1));
-      setProductExist(false);
-      setLoadingAction(false);
-    }
-  };
-
-  const onAction = () => {
-    if (!loadingAction) {
-      if (productExist) {
-        handleRemoveFromCart();
-      } else {
-        handleAddToCart();
-      }
-    }
-  };
-
-  useEffect(() => {
-    setProduct(detailsData);
-  }, [detailsData]);
-
-  /* check if the product has been added to cart */
-  const verifyProduct = async () => {
-    if (product !== null && uid) {
-      const { _id } = product;
-
-      const { exist } = await startIsProductAddedToCart(
-        _id,
-        uid
-      );
-
-      setProductExist(exist);
-    }
-  };
-
-  useEffect(() => {
-    verifyProduct();
-  }, [product]);
 
   const toggleShow = () => setShowModal(!showModal);
+
+  const { loadingAction, productExist, onAction } =
+    useCart(setShowModal);
 
   return (
     <div>
@@ -97,7 +23,7 @@ function ShoppingCartBtn() {
 
       <MDBBtn
         size="sm"
-        disabled={loadingAction ? true : false}
+        disabled={loadingAction}
         rounded
         outline
         color={productExist ? 'danger' : 'dark'}
