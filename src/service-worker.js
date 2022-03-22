d08d847e320f1a5c92afdfe154f4798bb1f30e20
Josue Rivas <reycoc2311@gmail.com>
2022-03-21 19:55:09 -0600

    files deleted and mofied

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,7 +9,10 @@
 
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import {
+  precacheAndRoute,
+  createHandlerBoundToURL,
+} from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
@@ -43,6 +46,7 @@ registerRoute(
 
     return true;
   },
+  // eslint-disable-next-line no-undef
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
@@ -50,7 +54,9 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) =>
+    url.origin === self.location.origin &&
+    url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
