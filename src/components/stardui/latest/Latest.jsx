d39a25369dc32752dcc5a0f55e3c7922797899db
Josue Rivas <reycoc2311@gmail.com>
2022-03-21 18:38:56 -0600

    feat(latest): resposive

--- src/components/stardui/latest/Latest.jsx
+++ src/components/stardui/latest/Latest.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CarouselLatest from '../carousel/carouselLatest/CarouselLatest';
 
 import RECOMMENDED from '../../../assets/img/recommended.svg';
+import BLOB from '../../../assets/img/blob.svg';
 import './Latest.css';
 
 function Latest() {
@@ -10,12 +11,21 @@ function Latest() {
     <section className="latest-container container my-5">
       <div className="row">
         <div className="latest-left col-lg-6">
-          <img
-            className="img-fluid w-50"
-            src={RECOMMENDED}
-            alt=""
-          />
-          <span>Recomendaciones para ti</span>
+          <section className="blod-container">
+            <div>
+              <img src={BLOB} alt="blod" className="blod" />
+            </div>
+
+            <div className="blod-info">
+              <img
+                className="img-fluid w-100"
+                src={RECOMMENDED}
+                alt=""
+              />
+
+              <span>Recomendaciones para ti</span>
+            </div>
+          </section>
         </div>
 
         <div className="latest-right col-lg-6">
