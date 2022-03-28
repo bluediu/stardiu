c5a21f257cae4705dc6e2bf162b20dfca0e50093
Josue Rivas <reycoc2311@gmail.com>
2022-03-28 12:22:28 -0600

    feat(categories): show cards

--- src/components/categories/CategoriesCard/CategoriesCard.jsx
+++ src/components/categories/CategoriesCard/CategoriesCard.jsx
@@ -1,42 +1,50 @@
 import React from 'react';
-import CATEGORIES_BG from '../../../assets/img/categories.jpg';
 
 import { Link } from 'react-router-dom';
-import { MDBBtn, MDBCard, MDBIcon } from 'mdb-react-ui-kit';
 
+import CATEGORIES_BG from '../../../assets/img/categories.jpg';
+import { MDBBtn, MDBCard, MDBIcon } from 'mdb-react-ui-kit';
 import './CategoriesCard.css';
 
-function CategoriesCard({ url }) {
+function CategoriesCard({ url, records }) {
   return (
-    <MDBCard>
-      <div
-        style={{
-          backgroundImage: `url(${CATEGORIES_BG})`,
-        }}
-        className="categories-blur-img"
-      ></div>
+    <>
+      {records.map((record) => (
+        <MDBCard key={record._id}>
+          <div
+            style={{
+              backgroundImage: `url(${CATEGORIES_BG})`,
+            }}
+            className="categories-blur-img"
+          ></div>
 
-      <div className="categories-info">
-        <div className="categories-info-blur">
-          <span>Hot Cofffes</span>
-        </div>
-      </div>
+          <div className="categories-info">
+            <div className="categories-info-blur">
+              <span>{record.name}</span>
+            </div>
+          </div>
 
-      <div className="categories-link">
-        <Link to={`${url}/hot-coffes`}>
-          <MDBBtn
-            size="md"
-            outline
-            rounded
-            color="light"
-            className="borders"
-          >
-            <MDBIcon className="me-2" fas icon="hat-wizard" />
-            Descubrir
-          </MDBBtn>
-        </Link>
-      </div>
-    </MDBCard>
+          <div className="categories-link">
+            <Link to={`${url}/${record.name}`}>
+              <MDBBtn
+                size="md"
+                outline
+                rounded
+                color="light"
+                className="borders"
+              >
+                <MDBIcon
+                  className="me-2"
+                  fas
+                  icon="hat-wizard"
+                />
+                Descubrir
+              </MDBBtn>
+            </Link>
+          </div>
+        </MDBCard>
+      ))}
+    </>
   );
 }
 
--- src/components/categories/CategoriesNavigation.jsx
+++ src/components/categories/CategoriesNavigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Link,
   Redirect,
@@ -6,12 +6,36 @@ import {
   Switch,
   useRouteMatch,
 } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
 import CategoriesCard from './CategoriesCard';
 import Fade from 'react-reveal/Fade';
 import '../menu/card/Card.css';
+import { MDBSpinner } from 'mdb-react-ui-kit';
+import { startGetAllCategories } from '../../context/actions/category.action';
+import CardItem from '../menu/card/CardItem';
 
 function CategoriesNavigation() {
   const { path, url } = useRouteMatch();
+
+  const dispatch = useDispatch();
+  const { records, isLoading } = useSelector(
+    (state) => state.categories
+  );
+
+  useEffect(() => {
+    dispatch(startGetAllCategories());
+  }, []);
+
+  if (isLoading)
+    return (
+      <div className="d-flex justify-content-center mt-5">
+        <MDBSpinner color="dark">
+          <span className="visually-hidden">Loading...</span>
+        </MDBSpinner>
+      </div>
+    );
 
   return (
     <div>
@@ -27,17 +51,23 @@ function CategoriesNavigation() {
 
           <hr />
           <section className="cards-grid categories-mg">
-            <CategoriesCard url={url} />
-            <CategoriesCard url={url} />
-            <CategoriesCard url={url} />
-            <CategoriesCard url={url} />
-            <CategoriesCard url={url} />
+            <CategoriesCard url={url} records={records} />
           </section>
         </Route>
 
-        <Route exact path={`${path}/hot-coffes`}>
-          <h1>HOT COFFES</h1>
-        </Route>
+        {/* <CardItem
+            {..record}
+            /> */}
+
+        {records?.map((record) => (
+          <Route
+            key={record._id}
+            exact
+            path={`${path}/${record.name}`}
+          >
+            <h3>{record.name}</h3>
+          </Route>
+        ))}
 
         <Redirect to={`${path}`} />
       </Switch>
