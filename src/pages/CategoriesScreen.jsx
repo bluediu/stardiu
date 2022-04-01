b9cccf93562ae7a3daf9e821f4b3a203e1307278
Josue Rivas <reycoc2311@gmail.com>
2022-04-01 17:59:42 -0600

    feat(document): tab title added in each section

--- src/pages/CategoriesScreen.jsx
+++ src/pages/CategoriesScreen.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 /* Components */
 import { Footer, Navbar } from '../components/stardui';
 import CategoriesNavigation from '../components/categories';
 
 function CategoriesScreen() {
+  useEffect(() => {
+    document.title = 'Stardui - Categories';
+  }, []);
+
   return (
     <>
       <Navbar />
--- src/pages/MenuScreen.jsx
+++ src/pages/MenuScreen.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 /* Components */
 import Menu from '../components/menu/Menu';
 import { Footer, Navbar } from '../components/stardui';
 
 function MenuScreen() {
+  useEffect(() => {
+    document.title = 'Stardui - Menu';
+  }, []);
+
   return (
     <>
       <Navbar />
