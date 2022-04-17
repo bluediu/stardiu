74a2ec5e63c5834c5843c03bee69d9610d1c009f
Josue Rivas <reycoc2311@gmail.com>
2022-04-16 19:00:09 -0600

    feat(payment): payment method implemented

---
+++ src/components/payment/Payment.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { usePayment } from '../../hooks/usePayment';
+
+/* Components */
+import {
+  MDBBtn,
+  MDBModal,
+  MDBModalBody,
+  MDBModalContent,
+  MDBModalDialog,
+  MDBModalHeader,
+  MDBModalTitle,
+} from 'mdb-react-ui-kit';
+import PaymentCard from './PaymentCard';
+
+import PropTypes from 'prop-types';
+
+function Payment({ show, toggleShow }) {
+  const {
+    handleCallback,
+    handleSubmit,
+    handleInputFocus,
+    handleInputChange,
+    creditCard,
+  } = usePayment(toggleShow);
+
+  const { number, name, expiry, cvc, issuer, focused, address } =
+    creditCard;
+
+  return (
+    <MDBModal staticBackdrop tabIndex="-1" show={show}>
+      <MDBModalDialog>
+        <MDBModalContent>
+          <MDBModalHeader>
+            <MDBModalTitle>Método de pago</MDBModalTitle>
+            <MDBBtn
+              className="btn-close"
+              color="none"
+              onClick={toggleShow}
+            ></MDBBtn>
+          </MDBModalHeader>
+          <MDBModalBody>
+            <PaymentCard
+              number={number}
+              name={name}
+              expiry={expiry}
+              cvc={cvc}
+              focused={focused}
+              callback={handleCallback}
+              handleSubmit={handleSubmit}
+              handleInputFocus={handleInputFocus}
+              handleInputChange={handleInputChange}
+              issuer={issuer}
+              defaultAddress={address}
+            />
+          </MDBModalBody>
+        </MDBModalContent>
+      </MDBModalDialog>
+    </MDBModal>
+  );
+}
+
+Payment.propTypes = {
+  show: PropTypes.bool.isRequired,
+  setShow: PropTypes.func.isRequired,
+};
+
+export default Payment;
