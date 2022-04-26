b20231ce5b5d01251ece0cd886ad37efb129b61e
Josue Rivas <reycoc2311@gmail.com>
2022-04-26 11:42:50 -0600

    index added for pages

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import AboutScreen from './AboutScreen';
+import CategoriesScreen from './CategoriesScreen';
+import DetailsScreen from './DetailsScreen';
+import HomeScreen from './HomeScreen';
+import ProfileScreen from './ProfileScreen';
+import ShoppingCartScreen from './ShoppingCartScreen';
+import MenuScreen from './MenuScreen';
+
+export {
+  AboutScreen,
+  CategoriesScreen,
+  DetailsScreen,
+  HomeScreen,
+  ProfileScreen,
+  ShoppingCartScreen,
+  MenuScreen,
+};
