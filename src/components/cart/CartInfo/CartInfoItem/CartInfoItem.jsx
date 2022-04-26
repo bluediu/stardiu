b0ceeb2f65b49622fcf37fe9ec0ce5320d5e92dd
Josue Rivas <reycoc2311@gmail.com>
2022-04-26 12:33:34 -0600

    animation implemented

--- src/components/cart/CartInfo/CartInfoItem/CartInfoItem.jsx
+++ src/components/cart/CartInfo/CartInfoItem/CartInfoItem.jsx
@@ -11,6 +11,7 @@ import {
   MDBSpinner,
 } from 'mdb-react-ui-kit';
 import CartRemoveBtn from '../CartRemoveBtn/CartRemoveBtn';
+import Fade from 'react-reveal/Fade';
 
 import PropTypes from 'prop-types';
 
@@ -36,73 +37,79 @@ function CartInfoItem({ productId, quantity, size, id }) {
   };
 
   return (
-    <div className="info-product mt-2">
-      <div className="product-detail">
-        <img src={productId.img} className="info-image" alt="" />
+    <Fade>
+      <div className="info-product mt-2">
+        <div className="product-detail">
+          <img
+            src={productId.img}
+            className="info-image"
+            alt=""
+          />
 
-        <div className="info-details">
-          <span>
-            <b>{productId.name}</b>
-          </span>
-          <span>
-            <b>Size:</b> {size}
-          </span>
+          <div className="info-details">
+            <span>
+              <b>{productId.name}</b>
+            </span>
+            <span>
+              <b>Size:</b> {size}
+            </span>
 
-          <span>
-            <CartRemoveBtn productId={productId} id={id} />
-          </span>
-        </div>
-
-        <div className="price-details">
-          <div className="product-amount-container">
-            <div className="product-amount">
-              {updatingQt ? (
-                <MDBSpinner size="sm">
-                  <span className="visually-hidden">
-                    Loading...
-                  </span>
-                </MDBSpinner>
-              ) : (
-                counter
-              )}
-            </div>
-            <MDBBtnGroup size="sm">
-              <MDBBtn
-                color="dark"
-                outline
-                style={{ border: '0' }}
-                onClick={() => {
-                  increaseBy(1);
-                  handleUpdate(counter + 1);
-                }}
-                disabled={isMaxReached() || updatingQt}
-              >
-                <span className={isMaxReached() && 'disabled'}>
-                  +
-                </span>
-              </MDBBtn>
-              <MDBBtn
-                color="dark"
-                outline
-                style={{ border: 0 }}
-                onClick={() => {
-                  increaseBy(-1);
-                  handleUpdate(counter - 1);
-                }}
-                disabled={counter === 1 || updatingQt}
-              >
-                <span className={counter === 1 && 'disabled'}>
-                  -
-                </span>
-              </MDBBtn>
-            </MDBBtnGroup>
+            <span>
+              <CartRemoveBtn productId={productId} id={id} />
+            </span>
           </div>
-          <div className="product-price">
-            {formatPrice.format(productId.price)}
+
+          <div className="price-details">
+            <div className="product-amount-container">
+              <div className="product-amount">
+                {updatingQt ? (
+                  <MDBSpinner size="sm">
+                    <span className="visually-hidden">
+                      Loading...
+                    </span>
+                  </MDBSpinner>
+                ) : (
+                  counter
+                )}
+              </div>
+              <MDBBtnGroup size="sm">
+                <MDBBtn
+                  color="dark"
+                  outline
+                  style={{ border: '0' }}
+                  onClick={() => {
+                    increaseBy(1);
+                    handleUpdate(counter + 1);
+                  }}
+                  disabled={isMaxReached() || updatingQt}
+                >
+                  <span className={isMaxReached() && 'disabled'}>
+                    +
+                  </span>
+                </MDBBtn>
+                <MDBBtn
+                  color="dark"
+                  outline
+                  style={{ border: 0 }}
+                  onClick={() => {
+                    increaseBy(-1);
+                    handleUpdate(counter - 1);
+                  }}
+                  disabled={counter === 1 || updatingQt}
+                >
+                  <span className={counter === 1 && 'disabled'}>
+                    -
+                  </span>
+                </MDBBtn>
+              </MDBBtnGroup>
+            </div>
+            <div className="product-price">
+              {formatPrice.format(productId.price)}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 }
 
