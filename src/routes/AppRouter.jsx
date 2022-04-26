c9066864761dcf28aa2c6228a591f56c3cea7a3a
Josue Rivas <reycoc2311@gmail.com>
2022-04-26 11:42:36 -0600

    refactor(routes): lazy load removed by performance issues

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import { ROUTE } from '../constants/auth-routes';
@@ -9,21 +9,20 @@ import { startCheckingRenewToken } from '../context/actions/auth.action';
 
 /* components */
 import Error404 from '../components/utils/Error404';
-import MainLoader from '../components/utils/main-loader/MainLoader';
 import PrivateRoute from './PrivateRoute';
-import ShoppingCartScreen from '../pages/ShoppingCartScreen';
 import { startCountProducts } from '../context/actions/shoppingCart';
-import CategoriesScreen from '../pages/CategoriesScreen';
-import ProfileScreen from '../pages/ProfileScreen';
 
 /* Lazy Load */
-const AboutScreen = lazy(() => import('../pages/AboutScreen'));
-const HomeScreen = lazy(() => import('../pages/HomeScreen'));
-const MenuScreen = lazy(() => import('../pages/MenuScreen'));
-const AuthRouter = lazy(() => import('./AuthRouter'));
-const DetailsScreen = lazy(() =>
-  import('../pages/DetailsScreen')
-);
+import {
+  AboutScreen,
+  CategoriesScreen,
+  DetailsScreen,
+  HomeScreen,
+  ProfileScreen,
+  ShoppingCartScreen,
+  MenuScreen,
+} from '../pages';
+import AuthRouter from './AuthRouter';
 
 function AppRouter() {
   const dispatch = useDispatch();
@@ -42,52 +41,47 @@ function AppRouter() {
   return (
     <HashRouter>
       <div>
-        <Suspense fallback={<MainLoader />}>
-          <Switch>
-            <Route exact path="/" component={HomeScreen} />
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
 
-            <PrivateRoute
-              path={ROUTE.AUTH_BASE}
-              component={AuthRouter}
-              isAuthenticated={!!uid}
-            />
+          <PrivateRoute
+            path={ROUTE.AUTH_BASE}
+            component={AuthRouter}
+            isAuthenticated={!!uid}
+          />
 
-            <PrivateRoute
-              path={ROUTE.CART}
-              component={ShoppingCartScreen}
-              isAuthenticated={!uid}
-            />
+          <PrivateRoute
+            path={ROUTE.CART}
+            component={ShoppingCartScreen}
+            isAuthenticated={!uid}
+          />
 
-            <Route
-              path={ROUTE.MENU_BASE}
-              component={MenuScreen}
-            />
+          <Route path={ROUTE.MENU_BASE} component={MenuScreen} />
 
-            <Route
-              exact
-              path={ROUTE.ABOUT}
-              component={AboutScreen}
-            />
+          <Route
+            exact
+            path={ROUTE.ABOUT}
+            component={AboutScreen}
+          />
 
-            <Route
-              exact
-              path={ROUTE.DETAILS_NAME}
-              component={DetailsScreen}
-            />
+          <Route
+            exact
+            path={ROUTE.DETAILS_NAME}
+            component={DetailsScreen}
+          />
 
-            <Route
-              path={ROUTE.CATEGORIES}
-              component={CategoriesScreen}
-            />
+          <Route
+            path={ROUTE.CATEGORIES}
+            component={CategoriesScreen}
+          />
 
-            <Route
-              path={ROUTE.PROFILE}
-              component={ProfileScreen}
-            />
+          <Route
+            path={ROUTE.PROFILE}
+            component={ProfileScreen}
+          />
 
-            <Route path="*" component={Error404} />
-          </Switch>
-        </Suspense>
+          <Route path="*" component={Error404} />
+        </Switch>
       </div>
     </HashRouter>
   );
