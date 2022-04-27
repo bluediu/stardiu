38cdb8f5af12bf074706e22f48cac6507b5af4d2
Josue Rivas <reycoc2311@gmail.com>
2022-04-26 18:38:23 -0600

    feat(utils): new components

---
+++ src/components/utils/Empty/Empty.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+/* Components */
+import { MDBBtn } from 'mdb-react-ui-kit';
+import Fade from 'react-reveal/Fade';
+import { ROUTE } from '../../../constants/auth-routes';
+
+import './Empty.css';
+
+function Empty({ title }) {
+  let history = useHistory();
+
+  return (
+    <div className="empty mt-5">
+      <h5 className="text-black-50">
+        {title},
+        <br />
+        comienza viendo nuestros productos
+      </h5>
+
+      <Fade>
+        <MDBBtn
+          rounded
+          outline
+          color="dark"
+          className="mt-3"
+          onClick={() => history.push(ROUTE.MENU_BASE)}
+        >
+          Descubrir
+        </MDBBtn>
+      </Fade>
+    </div>
+  );
+}
+
+export default Empty;
