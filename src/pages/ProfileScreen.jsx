8a73cea8543c6a908d0a9204915be2fb41f04e31
Josue Rivas <reycoc2311@gmail.com>
2022-05-13 11:57:24 -0600

    feat(profile): browser title added

--- src/pages/ProfileScreen.jsx
+++ src/pages/ProfileScreen.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import Profile from '../components/profile/Profile';
+import React, { useEffect } from 'react';
 
 /* Components */
+import Profile from '../components/profile/Profile';
 import Footer from '../components/stardui/footer/Footer';
 import Navbar from '../components/stardui/navbar/Navbar';
 
 function ProfileScreen() {
+  useEffect(() => {
+    document.title = 'Stardui - Profile';
+  }, []);
+
   return (
     <>
       <Navbar />
